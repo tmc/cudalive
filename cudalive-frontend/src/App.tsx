272c7fc02ca2a55eefe56d002afd4d1fcb9e65af
Travis Cline <travis.cline@gmail.com>
2024-05-27 22:54:27 -0700

    app: Add nav

--- cudalive-frontend/src/App.tsx
+++ cudalive-frontend/src/App.tsx
@@ -7,12 +7,35 @@ import ExampleCompletion from './components/ExampleCompletion'
 
 import { Button } from "@/components/ui/button"
 
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+
 
 function App() {
   return (
     <>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <h1>CUDAlive</h1>
+<NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
+
       <ModeToggle />
       <ErrorBoundary fallback={<p>Something went wrong</p>}>
         <Button>Click me</Button>
