d7ed93a4b17b9c3882cc4a628b4fa0b88932e74e
Travis Cline <travis.cline@gmail.com>
2024-05-27 22:32:00 -0700

    frontend: Add components

--- cudalive-frontend/src/App.tsx
+++ cudalive-frontend/src/App.tsx
@@ -3,14 +3,17 @@ import { ErrorBoundary } from "react-error-boundary";
 import './App.css'
 import ExampleCompletion from './components/ExampleCompletion'
 
+import { Button } from "@/components/ui/button"
+
 
 function App() {
   return (
     <>
       <h1>CUDAlive</h1>
       <ErrorBoundary fallback={<p>Something went wrong</p>}>
-      <ExampleCompletion />
-</ErrorBoundary>
+        <Button>Click me</Button>
+        <ExampleCompletion />
+      </ErrorBoundary>
     </>
   )
 }
